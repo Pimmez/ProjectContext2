2b13793ce3a27aa313ceed1a92a8286abb30211e
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-02-04 23:26:41 +0100

    -Added TextMeshPor -Added GameScene -Added GameLogic -Added CardLogic -Added Sprite (Square)

---
+++ DrDating/Assets/Scenes/CardLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardLogic : MonoBehaviour
+{
+	public bool isMouseOver = false;
+	[SerializeField] private TextMeshProUGUI text;
+
+	private void OnMouseOver()
+	{
+		isMouseOver = true;
+	}
+
+	private void OnMouseExit()
+	{
+		isMouseOver = false;
+	}
+
+	public void InduceRight()
+	{
+		text.text = "You have swiped right!";
+	}
+
+	public void InduceLeft()
+	{
+		text.text = "You have swiped left!";
+	}
+}
