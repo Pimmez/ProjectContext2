3a3dae32c8113796e885bd51de42f25bb6f1f616
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-02-06 16:52:48 +0100

    -Added a reset button

---
+++ DrDating/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour
+{
+	public void ResetScene(string _sceneName)
+	{
+		SceneManager.LoadScene(_sceneName);
+	}
+}
